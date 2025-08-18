afde20ea05d724fe33e0db404fe28b5b572cafa6
Ratnesh Kumar Sharma <rksiitd123@gmail.com>
2025-08-19 05:12:39 +0530

    redirection

---
+++ app/bodhmanthan-results/page.tsx
@@ -0,0 +1,6 @@
+// app/page.tsx
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("https://dbggurukulam.com/results/2025-26/bodha-manthan/First%20Term%20-%20July%202025");
+}
